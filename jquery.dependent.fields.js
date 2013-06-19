822879caabe6314fe59d70ecd974a94593bf997b
aaandre <aaandre@gmail.com>
2013-06-18 18:33:36 -0600

    Updated to work with multiple selections and added a parameter for a custom enclosing tag, span by default.

--- jquery.dependent.fields.js
+++ jquery.dependent.fields.js
@@ -1,31 +1,52 @@
-(function( $ ){
+(function($){
 
-  $.fn.dependsOn = function(element, value) {
+  $.fn.dependsOn = function(element, value, enclosing_tag) {
+    // enclosing tag is span by default
+    if (!enclosing_tag) {
+      enclosing_tag = 'span';
+    }
     var elements = this;
     //add change handler to element
-    $(element).change(function(){
+    $(element).change(function(){        
       var $this = $(this);
       var showEm = false;
       if ( $this.is('input[type="checkbox"]') ) {
         showEm = $this.is(':checked');
+        } else if ( $this.is('input[type="radio"]') ) {
+        showEm = $this.is(':checked');
       } else if ( $this.is('select') ) {
-        var fieldValue = $this.find('option:selected').val();
-        var showEm = false;
-        if ( !value ) {
+        // var fieldValue = $this.find('option:selected').val();
+        var fieldValue = $this.val();
+        
+        if ( !value ) {          
           showEm = fieldValue && $.trim(fieldValue) != '';
-        } else if (typeof(value) === 'string') {
-          showEm = value == fieldValue;
+        } else if (typeof(value) === 'string') {                        
+
+          // check for value in multiple selection                      
+          if ($.isArray(fieldValue)) {                        
+              showEm = ($.inArray(value, fieldValue) !== -1);
+          } else {
+              showEm = value == fieldValue;    
+          }
+          
         } else if ($.isArray(value)) {
-          showEm = ($.inArray(fieldValue, value) !== -1);
+            // check for multiple values
+            if ($.isArray(fieldValue)) {
+              // in multiple selection 
+              showEm = $.grep(fieldValue,function(el,i){return $.inArray(el,value) !== -1}).length > 0;
+            } else {
+              // in single selected item
+              showEm = ($.inArray(fieldValue, value) !== -1);
+            }          
         }
       }
-      elements.closest('p').toggle(showEm);
+      elements.closest(enclosing_tag).toggle(showEm);
     });
 
     //hide the dependent fields
     return elements.each(function(){
       var $this = $(this);
-      $this.closest('p').hide();
+      $this.closest(enclosing_tag).hide();
     });
   };
 })( jQuery );
